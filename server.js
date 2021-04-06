a4ffcfcecc5f105341e6b48778f1248eae08b785
Tomas Dostal <tomas@coyote.co.uk>
2021-04-07 00:11:09 +0100

    [Node] Implements rockets endpoint

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+const handleApiError = (error, res) => {
+  res.status(500).send({
+    message: `Something has gone wrong at SpaceX: ${error.response.data}`
+  });
+};
+
 app.use(function(req, res, next) {
+  // @NOTE: Access control origin would be set appropriately on production product
   res.header("Access-Control-Allow-Origin", "*"); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -30,12 +37,36 @@ app.get("/launches", async (req, res) => {
     );    
     res.send(response.data.docs);
   } catch(error) {
-    res.status(500).send({
-      message: `Something has gone wrong at SpaceX: ${error.response.data}`
-    });
+    handleApiError(error, res);
   }
-  
-  
+});
+
+app.get("/rockets", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://api.spacexdata.com/v4/rockets/query", 
+      {
+        query: {},
+        options: {
+          select: ["name", "description", "cost_per_launch", "flickr_images"]
+        }
+      }
+    );    
+
+    const parsedData = response.data.docs.map(rocket => ({
+      name: rocket.name,
+      description: rocket.description,
+      cost_per_launch: rocket.cost_per_launch.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }),
+      image: rocket.flickr_images && rocket.flickr_images.length ? rocket.flickr_images[0] : ''
+    }));
+
+    res.send(parsedData);
+  } catch(error) {
+    handleApiError(error, res);
+  }
 });
 
 app.listen(port, () => {
